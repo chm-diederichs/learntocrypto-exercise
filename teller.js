cbe2917e4b15dda10e713570aabb43a6883c9823
Christophe Diederichs <chris.d.itunes@gmail.com>
2019-04-24 16:08:28 +0200

    encrypt new user secret keys before saving to file

--- teller.js
+++ teller.js
@@ -8,6 +8,7 @@ var command = process.argv[2]
 var order
 var customer
 var hashCount
+var tellerKey = sodium.sodium_malloc(sodium.crypto_secretbox_KEYBYTES)
 
 var client = jsonStream(net.connect(3876))
 
@@ -22,6 +23,15 @@ try {
   } else {
     if (err) throw err
   }
+}
+
+try {
+  tellerKey.fill(fs.readFileSync('tellerKey.symm', 'ascii'), 'hex')
+} catch (err) {
+  sodium.randombytes_buf(tellerKey)
+  fs.writeFile('tellerKey.symm', tellerKey.toString('hex'), function (err) {
+    if (err) throw err
+  })
 }
 
 if (command === 'register') {
@@ -40,8 +50,18 @@ if (command === 'register') {
     var filePublic = fileNamePublic.replace('user', customer)
     var fileSecret = fileNameSecret.replace('user', customer)
 
-    var secretKey = fs.readFileSync(fileSecret, 'ascii')
+    var encryptedKey = JSON.parse(fs.readFileSync(fileSecret, 'ascii'))
     var publicKey = fs.readFileSync(filePublic, 'ascii')
+
+    var nonce = Buffer.from(encryptedKey.nonce, 'hex')
+    var cipher = Buffer.from(encryptedKey.cipher, 'hex')
+    var secretKey = sodium.sodium_malloc(cipher.length - sodium.crypto_secretbox_MACBYTES)
+
+    sodium.sodium_mprotect_readwrite(secretKey)
+    sodium.sodium_mprotect_readonly(tellerKey)
+    sodium.crypto_secretbox_open_easy(secretKey, cipher, nonce, tellerKey)
+    sodium.sodium_mprotect_noaccess(tellerKey)
+    sodium.sodium_mprotect_noaccess(secretKey)
 
     if (command === 'balance') {
       order = { cmd: command, customerNumber: customer, customerId: publicKey, 'hashCount': hashCount }
@@ -49,29 +69,36 @@ if (command === 'register') {
       order = { cmd: command, customerNumber: customer, customerId: publicKey, amount: parseInt(process.argv[3], 10), 'hashCount': hashCount }
     }
 
-    order.signature = bank.signToHex(JSON.stringify(order), Buffer.from(secretKey, 'hex'))
+    sodium.sodium_mprotect_readonly(secretKey)
+    order.signature = bank.signToHex(JSON.stringify(order), secretKey)
+    sodium.sodium_mprotect_noaccess(secretKey)
+
     client.end(order)
   } catch (err) {
     if (err.code === 'ENOENT') {
       throw new Error('User not registered.')
+    } else {
+      if (err) throw err
     }
   }
 }
 
 client.on('data', function (msg) {
   if (msg.hasOwnProperty('customerNumber')) {
-    sodium.sodium_mprotect_readwrite(newSecretKey)
     fileNameSecret = fileNameSecret.replace('user', msg.customerNumber)
     fileNamePublic = fileNamePublic.replace('user', msg.customerNumber)
 
-    fs.writeFile(fileNameSecret, newSecretKey.toString('hex'), function (err) {
+    sodium.sodium_mprotect_readwrite(newSecretKey)
+    sodium.sodium_mprotect_readonly(tellerKey)
+    fs.writeFile(fileNameSecret, JSON.stringify(bank.encryptToHex(newSecretKey, tellerKey)), function (err) {
       if (err) throw err
     })
+    sodium.sodium_mprotect_noaccess(tellerKey)
+    sodium.sodium_memzero(newSecretKey)
+
     fs.writeFile(fileNamePublic, newPublicKey.toString('hex'), function (err) {
       if (err) throw err
     })
-    sodium.sodium_memzero(newSecretKey)
-    sodium.sodium_munlock(newSecretKey)
   }
 
   console.log('Teller received:', msg)
